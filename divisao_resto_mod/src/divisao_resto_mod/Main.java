2ab531713e1a0946accd7f843dd922ff77a416bb
ronnie <ronnie.willyams.rws@gmail.com>
2025-02-09 22:00:45 -0300

    Vamos prosseguindo nos estudos de Java com Git.

--- divisao_resto_mod/src/divisao_resto_mod/Main.java
+++ divisao_resto_mod/src/divisao_resto_mod/Main.java
@@ -3,5 +3,14 @@ package divisao_resto_mod;
 public class Main {
     public static void main(String[] args) {
 		
+    	/*Estudo de módulo resto da divisao % */
+    	
+    	double carro = 9;
+    	double pessoa = 2;
+    	double resto = carro % pessoa;
+    	
+    	System.err.println("O resultado do resto da divisão "+resto);
+    	
+    	
 	}
 }
